140ca23f2b382ce3fb9ec18b17d5e7420c32c820
Ekrem <metrozi2018@Gmail.com>
2019-01-19 12:42:28 -0500

    dd one more method

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,6 +4,10 @@ public class Calculator {
         return num1+ num2;
     }
 
+    public static double subtract(double num1, double num2){
+        return num1 - num2;
+    }
+
     public static void main(String[] args) {
 
     }
