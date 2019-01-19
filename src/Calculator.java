0f119b3d428aca8521b76e1e59904b391f0623c1
Ekrem <metrozi2018@Gmail.com>
2019-01-19 12:54:14 -0500

    SUBTRact method added

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,10 +5,9 @@ public class Calculator {
     }
 
     public static double subtract(double num1, double num2){
-        return num1 - num2;
+        double sum = num1 - num2;
+        return sum;
     }
 
-    public static void main(String[] args) {
 
-    }
 }
